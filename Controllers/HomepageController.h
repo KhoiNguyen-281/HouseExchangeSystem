787e28a821401119751f35cd83a54d7fcdaaf3d5
KhoiNguyen-281 <75612025+KhoiNguyen-281@users.noreply.github.com>
2023-01-10 22:55:36 +0700

    create menu

--- Controllers/HomepageController.h
+++ Controllers/HomepageController.h
@@ -11,14 +11,49 @@
 namespace HomepageComponent {
     int optionInput();
 
-    void displayStartPage();
+    void displayStartPage(); //
+
+//    Guest menu
     void displayGuestHomepage();
+    // 0. Exit
+    //1. viewAllHouse
+    //2. register -> login page
+    //3. login
+
+    // wrong, navigate to signup page
+
+//    Member menu (new)
+    void newMemberMenu();
+    //0.  Exit
+    //1.  View Information
+    //2. List available houses. --> call registerNewHouse
+    //	3. Unlist current house -- call removeHouse
+    //	4. Request new house. --> call bookAccommodation
+    //	5. Search available house. --> display available house;
+
+    void oldMemberMenu();
+    //0.  Exit
+    //1.  View Information
+    //2. List available houses. --> call registerNewHouse
+    //	3. Unlist current house -- call removeHouse
+    //	4. Request new house. --> call bookAccommodation
+    //	5. Search available house. --> display available house;
+    // 6. View request (approve, deny);
+    // 7. Give feedback (occupier, house)
+
+    void ratingMenu();
+    // 0. Exit
+    // 1. Rate occupier
+    // 2. Rate house
+
+
+
 //    string fieldInput(std::string field);
     void displayRequestOption(Member *member, House* requestHouse);
     void displayHousesOption(Member* member);
     void displaySpecificHouseOption(); 
     void displayLocationOption();
-    void displayMemberHomepage(Member *member);
+
 
     void displayAdminHomepage();
     void displayAppHomepage();
